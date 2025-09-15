ea13b249c129efe6c28cdc1f2c744adb7fb7b38f
Mahmoud Mirghani <mahmoudtawfiq777@gmail.com>
2025-09-15 21:53:38 +0300

    Add a Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+import styles from "../styles/Home.module.css";
+
 export default function Home() {
     return (
-        <h1>Hello</h1>
+        <div className={styles.home}>
+            <p className={styles.title}>Welcome to our fake store.</p>
+            <p className={styles.text}>You can browse products in the shop tab or by clicking on the button below.</p>
+            <Link to="/shop" className={styles.button}>
+                Go to Shop
+            </Link>
+        </div>
     );
 }
